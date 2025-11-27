0fc864a73e5e69401cac32145f204861a84973c7
Papiizaza <papiizaza@gmail.com>
2025-11-27 08:38:01 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    title: 'Email Validator Pro'
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.removeMenu(); // Remove menu bar for cleaner look
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
